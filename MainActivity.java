360690d9674a10c7e024cede73515f4dcf7ddc77
Namrata-18 <waghnamrata11@gmail.com>
2016-11-05 11:30:07 +0530

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,35 +1,32 @@
 package com.example.yashd.pulseit;
 
 import android.content.ActivityNotFoundException;
-import android.content.BroadcastReceiver;
-import android.content.Context;
 import android.content.Intent;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
-import android.telephony.TelephonyManager;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
-
-import static com.example.yashd.pulseit.MainActivity.GPSTracker.MIN_DISTANCE_CHANGE_FOR_UPDATES;
-import static com.example.yashd.pulseit.MainActivity.GPSTracker.MIN_TIME_BW_UPDATES;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
+import android.app.Activity;
+import android.view.View;
 import static java.lang.Integer.parseInt;
 
 public class MainActivity extends AppCompatActivity {
-EditText pulse,due;
+    EditText pulse;
+    ImageView imageView;
+    ImageButton imgButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-         pulse = (EditText) findViewById(R.id.editText);
+        pulse = (EditText) findViewById(R.id.editText);
+       // imageView = (ImageView) findViewById(R.id.imageView2);
 
         Button submit = (Button) findViewById(R.id.button);
 
@@ -38,37 +35,51 @@ EditText pulse,due;
                 buttonOnClick();
             }
         });
-    }
 
-    public void buttonOnClick(){
-        String val = pulse.getText().toString();
-
-        if (parseInt(val)>220||parseInt(val)<40)
-        {
-            call();
-
-        }
-        else
-        {
-        pulse.setText("You have a normal heart rate :)");
-        }
-    }
-
-
-        private void call() {
-            try {
-                Intent callIntent = new Intent(Intent.ACTION_CALL);
-                callIntent.setData(Uri.parse("tel:7506945191"));
-                startActivity(callIntent);
-            } catch (ActivityNotFoundException activityException) {
-                Log.e("helloandroid dialing example", "Call failed", activityException);
+        imgButton =(ImageButton)findViewById(R.id.imageView2);
+        imgButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("clicks","You Clicked B1");
+                Intent i=new Intent(
+                        MainActivity.this,
+                        Main2Activity.class);
+                startActivity(i);
             }
-        }
+        });
+    }
 
+    public void buttonOnClick() {
+        String val = pulse.getText().toString();
+        imgButton.setImageResource(R.drawable.image2);
+
+
+        if (parseInt(val) > 220 || parseInt(val) < 40) {
+            call();
+        }
+        else {
+            pulse.setText("You have a normal heart rate :)");
+        }
+    }
+
+
+    private void call() {
+        try {
+            Intent callIntent = new Intent(Intent.ACTION_CALL);
+            callIntent.setData(Uri.parse("tel:7506945191"));
+            startActivity(callIntent);
+        } catch (ActivityNotFoundException activityException) {
+            Log.e("helloandroid dialing example", "Call failed", activityException);
+        }
+    }
+}
+
+
+/*
     public class CallDurationReceiver extends BroadcastReceiver {
 
-         boolean flag = false;
-         Long start_time, end_time;
+        boolean flag = false;
+        Long start_time, end_time;
 
         @Override
         public void onReceive(Context arg0, Intent intent) {
@@ -86,14 +97,14 @@ EditText pulse,due;
                     //Store total_time somewhere or pass it to an Activity using intent
                     setContentView(R.layout.activity_main);
                     TextView textView = (TextView) findViewById(R.id.duration);
-                   // textView.setText(String.valueOf(total_time));
-                    if(total_time>0)
-                    {
+                    // textView.setText(String.valueOf(total_time));
+                    if (total_time > 0) {
                         textView.setText("i sgreater than ");
                     }
                 }
             }
         }
-    }
+    }*/
+
 
 
